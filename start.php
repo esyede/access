c272648d41014a10b190b29beb2940ade9836870
esyede <suyadi.1992@gmail.com>
2020-12-20 19:40:17 +0700

    small changes

--- start.php
+++ start.php
@@ -1,7 +1,11 @@
 <?php
 
-Autoloader::namespaces(['Esyede\Access\Libraries' => Package::path('access').'models']);
-Autoloader::map(['Access' => __DIR__.DS.'libraries/Access'.EXT]);
+defined('DS') or exit('No direct script access.');
+
+Autoloader::namespaces(['Esyede\Access\Libraries' => Package::path('access').'libraries']);
+Autoloader::namespaces(['Esyede\Access\Models' => Package::path('access').'models']);
+Autoloader::map(['Access' => Package::path('access').'libraries'.DS.'Access'.EXT]);
+
 Auth::extend('access', function () {
     return new Access();
 });
