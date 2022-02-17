a6d2dde978fe49d5287f5c83c8d75854c4b91807
Suyadi <suyadi.1992@gmail.com>
2022-02-17 23:29:40 +0700

    Model: fix wrong variable name

--- models/role.php
+++ models/role.php
@@ -18,7 +18,7 @@ class Role extends Model
 
     public function set_slug($slug)
     {
-        $this->set_attribute('slug', Str::slug($password));
+        $this->set_attribute('slug', Str::slug($slug));
     }
 
     public function users()
