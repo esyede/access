370aadc5ada74c1626c08a652ee47ee1aff3cc14
Suyadi <suyadi.1992@gmail.com>
2022-11-30 12:53:50 +0700

    Initial commit

--- boot.php
+++ boot.php
@@ -2,9 +2,9 @@
 
 defined('DS') or exit('No direct script access.');
 
-Autoloader::namespaces(['Esyede\Access\Libraries' => Package::path('access').'libraries']);
-Autoloader::namespaces(['Esyede\Access\Models' => Package::path('access').'models']);
-Autoloader::map(['Access' => Package::path('access').'libraries'.DS.'Access.php']);
+Autoloader::namespaces(['Esyede\Access\Libraries' => __DIR__.DS.'libraries']);
+Autoloader::namespaces(['Esyede\Access\Models' => __DIR__.DS.'models']);
+Autoloader::map(['Access' => __DIR__.DS.'libraries'.DS.'Access.php']);
 
 Auth::extend('access', function () {
     return new Access();
